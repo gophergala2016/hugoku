f7cf9b8c1dd4d0c4711e88eb9093a67dcfba67f6
Pablo de la Concepción Sanz <pconcepcion@strands.com>
2016-01-24 16:58:43 +0100

    Added stored basic Description on project, time measures, Lint Fix

--- util/session/get_user.go
+++ util/session/get_user.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gophergala2016/hugoku/store"
 )
 
-// getUserFomSession gets the user from the usernane in the session on the http Request
+// GetUser gets the user from the usernane in the session on the http Request
 func GetUser(r *http.Request) (store.User, error) {
 	session := sessions.GetSession(r)
 	username := session.Get("username")
